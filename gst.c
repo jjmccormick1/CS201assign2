73c9fbc765c68a43a65701391358f53b28a4bff0
jjmccormick <jjmccormick@crimson.ua.edu>
2018-02-28 21:45:19 -0600

    Finishing gst

--- gst.c
+++ gst.c
@@ -5,6 +5,10 @@
 #include<stdio.h>
 #include "bst.h"
 
+GSTVALUE * newGSTVALUE(void * val);
+void swapperGST(BSTNODE *, BSTNODE *);
+int compareGST(void *, void *);
+
 struct gst{
     BST * bst;
     void (*display)(void *, FILE *);
@@ -15,20 +19,47 @@ struct gst{
 typedef struct gstvalue {
     void * value;
     int frequency;
+    void (*display)(void *, FILE *);
+    int (*compare)(void *, void *);
 }GSTVALUE;
 
 GST *newGST(void (*display)(void *,FILE *), int (*compare)(void *,void *), void (*free)(void *))
 {
     GST * gst = malloc(sizeof(GST));
+    gst->bst = newBST(display,compare, swapperGST , free);
     gst->display = display;
     gst->compare = comapre;
     gst->free = free;
 }
 
+GSTVALUE * newGSTVALUE(GST * gst,void * val)
+{
+    GSTVALUE * gstvalue = malloc(sizeof(GSTVALUE));
+    gstvalue->value = value;
+    gstvalue->frequency  = 1;
+    gstvalue->display = gst->display;
+    gstvalue->compare = gst->compare;
+}
+
+void swapperGST(BSTNODE * a, BSTNODE * b)
+{
+    GSTVALUE * gsta = (GSTVALUE*)getBSTNODEvalue(a);
+    GSTVALUE * gstb = (GSTVALUE*)getBSTNODEvalue(b);
+    void * tmp = gsta->value;
+    gsta->value = gstb->value;
+    gstb->value = tmp;
+}
+
+int compareGST(void * a, void * b)
+{
+    GSTVALUE * gsta = (GSTVALUE*)a;
+    GSTVALUE * gstb = (GSTVALUE*)b;
+    
+    return gsta->compare(gsta->value, gstb->value);
+}
 void insertGST(GST * gst,void * value)
 {
     BSTNODE * found = findBST(gst->bst, value); //See if its in the tree
-    
     
     if(found == NULL) //if not in tree, add it
     {
@@ -38,13 +69,31 @@ void insertGST(GST * gst,void * value)
         insertBST(gst->bst, gstvalue);
         return;
     }
-    
-    
-        
+    GSTVALUE * gstval = getBSTNODEvalue(found); //if is in tree, increment frequency
+    gstval->frequency += 1; 
 }
-    extern int findGSTcount(GST *,void *);
-    extern void *findGST(GST *,void *);
-    extern void *deleteGST(GST *,void *);
+int findGSTcount(GST * gst,void * value)
+{
+     BSTNODE * found = findBST(gst->bst, value); //See if its in the tree
+     
+     if(found == NULL)
+         return 0;
+     
+     GSTVALUE * gstval = getBSTNODEvalue(found);
+     return gstval->frequency;
+}
+
+void *findGST(GST * gst,void * value)
+{
+    BSTNODE * found = findBST(gst->bst, value);
+    if(found == NULL)
+        return NULL;
+    return value;
+}
+void *deleteGST(GST * gst,void * value)
+{
+    return deleteBST(gst->bst, value);
+}
     extern int sizeGST(GST *);
     extern int duplicates(GST *);
     extern void statisticsGST(GST *,FILE *);
