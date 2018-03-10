4b96d59b064a0f95c798400f82eb97054429d52e
jjmccormick <jjmccormick@crimson.ua.edu>
2018-03-10 14:12:49 -0600

    working on trees.c

--- trees.c
+++ trees.c
@@ -2,11 +2,14 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <string.h>
+#include "scanner.h"
 
 int isGST = 0;
 
 static int processOptions(int,char **);
 void Fatal(char *,...);
+char * getNext(FILE *);
+
 
 int
 main(int argc,char **argv)
@@ -14,7 +17,7 @@ main(int argc,char **argv)
     char * corpus = NULL;
     char * commands = NULL;
     
-    if(argc < 2 || argc > 3)
+    if(argc < 1 || argc > 3)
     {
         printf("Not right amount of arguments");
         return 0;
@@ -26,13 +29,72 @@ main(int argc,char **argv)
     }
     else
     {
+        if(strcmp(argv[0],"-v") == 0)
+        {
+            printf("Jeremy McCormick");
+            retirn 0;
+        }
         if(strcmp(argv[0],"-g") == 0)
             isGST = 1;
         corpus = argv[1];
         commands = argv[2];
     }
+    FILE * corpusfp = fopen(corpus, "r");
+    FILE commandsfp = fopen(commands, "r");
+    
+    if(commandsfp == NULL || corpusfp == NULL)
+    {
+        printf("Files not found");
+        return 0;
+    }
+    
+    char * incorpus = getNext(incorpus);
+    char incommand;
+    
+    while(!feof(incommand) && !feof(incorpus))
+    {
+        incorpus = getNext(incorpus);
+        incommand = readChar(incommands);
+        
+        switch(incommand)
+        {
+            case 'i': 
+                insertTree(incorpus);
+                incorpus = getNext(incorpus);
+                break;
+            case 'd':
+                deleteTree(incorpus);
+                incorpus = getNext(incorpus);
+                break;
+            case 'f':
+                frequencyTree(incorpus);
+                incorpus = getNext(incorpus);
+                break;
+            case 's':
+                showTree();
+                break;
+            case 'r':
+                statsTree();
+                break;
+            default:
+                printf("command not understood");
+        }
+        
+    
+    
 
     return 0;
 }
 
-
+char * getNext(FILE * in)
+{
+    if(stringPending(in))
+    {
+        return readString(in);
+    }
+    else
+    {
+        return readToken(in);
+    }
+    return NULL;
+}
